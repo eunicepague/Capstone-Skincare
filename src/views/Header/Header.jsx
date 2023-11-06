50443878afb5daf7a705502ab884c50db23adaa5
eunicepague <eunicegracepague@gmail.com>
2023-11-06 17:05:39 +0800

    header reverted

--- src/views/Header/Header.jsx
+++ src/views/Header/Header.jsx
@@ -1,32 +1,13 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
-import { useNavigate, Link } from "react-router-dom";
-import "./Header.css";
-import axios from "./../../axios";
-import { useState, useEffect } from "react";
+import { useState } from 'react';
+import { Nav, Navbar, Offcanvas } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
-  const [user, setUser] = useState({});
-  const navigate = useNavigate();
+  const [show, setShow] = useState(false);
 
-  const userLogged = async () => {
-    await axios
-      .get("/authUser")
-      .then((res) => {
-        setUser(res.data.user);
-      })
-      .catch((err) => {
-        setUser({});
-      });
-  };
-  useEffect(() => {
-    // Call userLogged when the component mounts
-    userLogged();
-  }, []);
-
-  const logout = async () => {
-    await axios.post("/logout");
-    setUser({});
-  };
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <>
@@ -34,18 +15,20 @@ const Header = () => {
         <h6>Free Shipping worldwide</h6>
       </div>
       <Navbar expand="lg" id="navbar-container">
-        <Container>
-          <Navbar.Brand as={Link} to="/">
-            The Ordinary.
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Brand as={Link} to="/">
+          The Ordinary.
+        </Navbar.Brand>
+        <Navbar.Toggle onClick={handleShow} aria-controls="basic-navbar-nav" />
+
+        {/* Conditionally render Navbar links based on the "show" state */}
+        {show ? null : (
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto mx-auto">
               <Nav.Link as={Link} to="/new">
                 New
               </Nav.Link>
-              <Nav.Link as={Link} to="/bestsellers">
-                Best Sellers
+              <Nav.Link as={Link} to="/ProductCategories">
+                Products
               </Nav.Link>
               <Nav.Link as={Link} to="/Account">
                 Skin Care
@@ -58,28 +41,51 @@ const Header = () => {
               </Nav.Link>
             </Nav>
             <Nav className="ml-auto">
-              {user.name ? (
-                <>
-                  <Nav.Link as={Link} to="/Account">
-                    {user.name}
-                  </Nav.Link>
-                  <Nav.Link onClick={logout}>Logout</Nav.Link>
-                </>
-              ) : (
-                <>
-                  <Nav.Link as={Link} to="/login">
-                    Log in
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/register">
-                    Register
-                  </Nav.Link>
-                </>
-              )}
-
-              {/* <box-icon name="cart"></box-icon> */}
+              <Nav.Link as={Link} to="/login">
+                Log in
+              </Nav.Link>
+              <Nav.Link as={Link} to="/register">
+                Register
+              </Nav.Link>
+              <box-icon name="cart"></box-icon>
             </Nav>
           </Navbar.Collapse>
-        </Container>
+        )}
+
+        {/* Offcanvas */}
+        <Offcanvas show={show} onHide={handleClose}>
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="me-auto mx-auto">
+              <Nav.Link as={Link} to="/new">
+                New
+              </Nav.Link>
+              <Nav.Link as={Link} to="/ProductCategories">
+                Products
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Account">
+                Skin Care
+              </Nav.Link>
+              <Nav.Link as={Link} to="/cart">
+                Cart
+              </Nav.Link>
+              <Nav.Link as={Link} to="/order">
+                Order
+              </Nav.Link>
+            </Nav>
+            <Nav className="ml-auto">
+              <Nav.Link as={Link} to="/login">
+                Log in
+              </Nav.Link>
+              <Nav.Link as={Link} to="/register">
+                Register
+              </Nav.Link>
+              <box-icon name="cart"></box-icon>
+            </Nav>
+          </Offcanvas.Body>
+        </Offcanvas>
       </Navbar>
     </>
   );

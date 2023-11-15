eb5f8a6d76f19a4d2ed9d7600afeb11ae2f0276b
eunicepague <eunicegracepague@gmail.com>
2023-11-16 03:21:00 +0800

    added order jsx for admin

--- src/views/Admin/sections/Order.jsx
+++ src/views/Admin/sections/Order.jsx
@@ -1,7 +1,39 @@
 // import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import './Order.css';
 
 const Order = () => {
-  return <div></div>;
+  return (
+    <Container fluid>
+      <section id="adminOrder-content">
+        <Row>
+          <h3>Dashboard</h3>
+          <Col>
+            <div className="admin-Order">
+              <table id="order-table">
+                <tr id="adminOrder-tableHead">
+                  <th>Order Id</th>
+                  <th style={{ width: '20rem' }}>Client Name</th>
+                  <th style={{ width: '20rem' }}>Client Email</th>
+                  <th style={{ width: '10rem' }}>Product Cost</th>
+                  <th style={{ width: '10rem' }}>Status</th>
+                  <th style={{ width: '10rem' }}>Date</th>
+                </tr>
+                <tr id="adminOrdertableBody">
+                  <td>sample sample sample</td>
+                  <td>sample sample sample</td>
+                  <td>sample sample sample</td>
+                  <td>sample sample sample</td>
+                  <td>sample sample sample</td>
+                  <td>sample sample sample</td>
+                </tr>
+              </table>
+            </div>
+          </Col>
+        </Row>
+      </section>
+    </Container>
+  );
 };
 
 export default Order;

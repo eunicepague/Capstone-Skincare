d1e64ba4826d8864d2e4225216e32afef816cfd4
eunicepague <eunicegracepague@gmail.com>
2023-11-16 17:08:44 +0800

    deleted react reveal

--- src/views/Home/sections/Sec2.jsx
+++ src/views/Home/sections/Sec2.jsx
@@ -1,7 +1,7 @@
 // import React from 'react'
 import { Component, useEffect, useState } from 'react';
 import { Container, Card, Row, Col, Button } from 'react-bootstrap';
-import Fade from 'react-reveal/Fade';
+// import Fade from 'react-reveal/Fade';
 
 import './Sec2.css';
 import axios from 'axios';
@@ -27,30 +27,29 @@ const Sec2 = () => {
       <section id="s2-container">
         <Container id="s2-content">
           <h1 id="s2-h1">THE ORDINARY BEST SELLERS</h1>
-          <Fade bottom>
-            <div id="s2-card-container">
-              <Row className="mx-auto">
-                {/* Map through the products and add a key prop */}
-                {products.slice(0, 4).map((item) => (
-                  <Col key={item.id} xs={6} sm={6} md={3} lg={3}>
-                    <Card id="s2-card-box" className="mx-auto mt-3 ">
-                      <Card.Img
-                        id="s2-cardimg"
-                        variant="top"
-                        src={`/src/assets/${item.image}`}
-                      />
-                      <Card.Body id="s2-cardbody">
-                        <Card.Text id="s2-cardtitle">
-                          {item.name}
-                          <Button id="s2-button">SHOP NOW</Button>
-                        </Card.Text>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                ))}
-              </Row>
-            </div>
-          </Fade>
+
+          <div id="s2-card-container">
+            <Row className="mx-auto">
+              {/* Map through the products and add a key prop */}
+              {products.slice(0, 4).map((item) => (
+                <Col key={item.id} xs={6} sm={6} md={3} lg={3}>
+                  <Card id="s2-card-box" className="mx-auto mt-3 ">
+                    <Card.Img
+                      id="s2-cardimg"
+                      variant="top"
+                      src={`/src/assets/${item.image}`}
+                    />
+                    <Card.Body id="s2-cardbody">
+                      <Card.Text id="s2-cardtitle">
+                        {item.name}
+                        <Button id="s2-button">SHOP NOW</Button>
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </div>
         </Container>
       </section>
     </>

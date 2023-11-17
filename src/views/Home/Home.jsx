966c36ff3ade57a13f0dd47578f40eec8a9c7080
eunicepague <eunicegracepague@gmail.com>
2023-11-17 10:00:04 +0800

    added last section on home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -3,6 +3,7 @@ import Sec1 from './sections/Sec1';
 import Sec2 from './sections/Sec2';
 import Sec3 from './sections/Sec3';
 import Sec4 from './sections/Sec4';
+import Sec5 from './sections/Sec5';
 
 const Home = () => {
   return (
@@ -14,6 +15,7 @@ const Home = () => {
       <Sec2 />
       <Sec3 />
       <Sec4 />
+      <Sec5 />
     </div>
   );
 };

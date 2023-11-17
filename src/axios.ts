5d5ad1c2019bbfa6920a162a7d1e10ac16780bf7
Nikooolle <valledorn01@gmail.com>
2023-11-17 18:39:05 +0800

    update axios

--- src/axios.ts
+++ src/axios.ts
@@ -53,15 +53,15 @@ axiosRequest.interceptors.response.use(
       // store.commit("SET_ERROR", 401);
       // router.push("/login");
       const currentURL = window.location.pathname;
-      if (
-        currentURL != "/login" &&
-        currentURL != "/" &&
-        window.localStorage.getItem("ECCOM_TOKEN") == null
-      ) {
-        window.location.href = "/login";
-      } else if (window.localStorage.getItem("ECCOM_TOKEN") != null) {
-        window.location.href = "/";
-      }
+      // if (
+      //   currentURL != "/login" &&
+      //   currentURL != "/" &&
+      //   window.localStorage.getItem("ECCOM_TOKEN") == null
+      // ) {
+      //   window.location.href = "/login";
+      // } else if (window.localStorage.getItem("ECCOM_TOKEN") != null) {
+      //   window.location.href = "/";
+      // }
     }
     if (error.response.status == 422) {
       const item = error.response.data.errors;
